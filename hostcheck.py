86275372e18d30692b196996f5161ca0d4ae1a47
Maxim Tolstov <146558812+redH0od@users.noreply.github.com>
2024-11-26 11:21:11 +1000

    Update hostcheck.py
    Решил все таки дать отображение отправление пользователя для полного контроля

--- hostcheck.py
+++ hostcheck.py
@@ -3,7 +3,7 @@ from scapy.all import ICMP, IP, sr1
 def ping_host(target):
     ip = IP(dst=target)
     icmp = ICMP()
-    response = sr1(ip/icmp, timeout=2, verbose=False)
+    response = sr1(ip/icmp, timeout=2)
     
     if response == None:
         print(f"Host {target} isn't available (timeout).")
