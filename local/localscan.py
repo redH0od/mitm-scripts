499bbc65984bf58d0cb97ce402df8ce1d0ead2e8
Maxim Tolstov <146558812+redH0od@users.noreply.github.com>
2024-11-27 23:29:09 +1000

    Update localscan.py
    Добавил вывод имени хоста для упрощения поиска

--- local/localscan.py
+++ local/localscan.py
@@ -10,13 +10,20 @@ def scan_network(network):
     for sent, received in responce:
         devices.append({'ip': received.psrc, 'mac': received.hwsrc})
 
+        try:
+            device_info['hostname'] = socket.gethostbyaddr(received.psrc)[0]
+        except socket.herror:
+            device_info['hostname'] = "Unknown"
+        
+        devices.append(device_info)
+
     return devices
 
 def display_devices(devices):
-    print("IP Address\t\tMAC Address")
+    print("IP Address\t\tMAC Address\t\tHostname")
     for device in devices:
-        print(f"{device['ip']}\t\t{device['mac']}")
+        print(f"{device['ip']}\t\t{device['mac']}\t{device['hostname']}")
 
-network = input("Enter network to scan: ") #Указывайте диапазон в маске!
+network = input("Enter network to scan: ") #Указывайте диапазон в маске (в конце /24 просто добавляйте)!
 devices = scan_network(network)
 display_devices(devices)
